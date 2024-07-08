c44250c7f05f49580c6971395ac73469588128db
Administrator <Administrator@LAB1-MAY34>
2024-07-08 10:03:16 +0700

    Add project files.

---
+++ baitap1/HocSinh.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitap1
+{
+    internal class HocSinh
+    {
+       
+        public string Mahs { get; set; }
+        public string Hoten { get; set; }
+        public string Phai { get; set; }
+        public string Diachi { get; set; }
+        public float Điemtoan { get; set; }
+        public float Diemhoa { get; set; }
+        public float Diemly { get; set; } 
+       
+       
+        public float Dtb
+        {
+            get
+            {
+                return (Điemtoan + Diemhoa + Diemly) / 3;
+
+            }
+        }
+        public HocSinh (string mahs, string hoten, string phai, string diachi, float toan, float hoa, float ly)
+        {
+           Mahs = mahs;
+            Hoten = hoten; 
+            Phai = phai;
+            Diachi = diachi;
+              Diemly = ly;
+            Diemhoa = hoa;
+            Điemtoan = toan;
+
+       
+
+
+    }
+}
+}
---
+++ baitap1/Program.cs
@@ -0,0 +1,37 @@
+﻿using baitap1;
+
+List<HocSinh> dshs = new List<HocSinh>();
+Console.Write("Nhap so luong hoc sinh:");
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Nhap thong tin hoc sinh thu" + (i + 1) + ":");
+    Console.Write("Ma hoc sinh:");
+    string mahs = Console.ReadLine();
+    Console.Write("Hoten Hoc sinh:");
+    string hoten = Console.ReadLine();
+    Console.Write("Mon Phai:");
+    string phai = Console.ReadLine();
+    Console.Write("Diachi:");
+    string diachi = Console.ReadLine();
+    Console.Write("Nhap diem mon toan");
+    float toan = float.Parse(Console.ReadLine());
+    Console.Write("Nhap diem mon hoa");
+    float hoa = float.Parse(Console.ReadLine());
+    Console.Write("Nhap diem mon ly");
+    float ly = float.Parse(Console.ReadLine());
+    HocSinh hs = new HocSinh(mahs, hoten, phai, diachi, toan, hoa, ly);
+    dshs.Add(hs); }
+    Console.WriteLine("Danh sach hoc sinh");
+        for(int i=0; i<n; i++)
+    {
+    Console.WriteLine(dshs[i].Mahs + "\t" + dshs[i].Hoten +"\t" + dshs[i].Phai + "\t" + dshs[i].Diachi + "\t" + dshs[i].Điemtoan + "\t" + dshs[i].Diemhoa + "\t" + dshs[i].Diemly + "\t" + dshs[i].Dtb); 
+    }  
+        
+
+
+
+  
+        
+          
+        
